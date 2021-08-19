fc643ffd80787848f371e01781ae577aadc01b20
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-20 00:54:33 +0530

    Add:Blur effect on form popup Implemented: 1.)Add:Div tag for blur container. Because:

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 function display_form(){
     document.getElementById("edit-form").style.display="block";
+
+    /*
+    Alternative syntax to blur bg.
+    document.querySelector('.blur').style.filter=`blur(${2}px)`;
+   */
+    document.getElementById('blur').style.filter=`blur(${2}px)`;
 }
