33b2fb584f79cc7f9377b2e488b089007feb38bf
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-22 21:34:14 +0530

    Add:Add task pop up form. Implemented:
    Because:

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-function display_form(){
-    document.getElementById("edit-form").style.display="block";
+function display_edit_form() {
+  document.getElementById("edit-form").style.display = "block";
 
-    /*
+  /*
     Alternative syntax to blur bg.
     document.querySelector('.blur').style.filter=`blur(${2}px)`;
    */
-    document.getElementById('blur').style.filter=`blur(${2}px)`;
+  document.getElementById("blur").style.filter = `blur(${2}px)`;
 }
 
-function hide_form(){
-    document.getElementById("edit-form").style.display="none";
-    document.getElementById('blur').style.filter=`blur(${0}px)`;
-
+function hide_edit_form() {
+  document.getElementById("edit-form").style.display = "none";
+  document.getElementById("blur").style.filter = `blur(${0}px)`;
+  document.getElementById("edit-form-title").innerHTML = "Edit Task";
 }
-function delete_card(){
-    confirm("Jab delete hi karna tha tho bananya ku bhai?");
+function delete_card() {
+  confirm("Jab delete hi karna tha tho bananya ku bhai?");
+}
+
+function display_add_task_form() {
+  document.getElementById("edit-form-title").innerHTML = "New Task";
+  document.getElementById("edit-form").style.display = "block";
+  document.getElementById("blur").style.filter = `blur(${2}px)`;
 }
