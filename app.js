ad016469097147f68930f7faced235192600abf6
Siddharth Teli <telisiddharth123@gmail.com>
2021-08-23 18:41:46 +0530

    Add:Dynamic card display with dummy data. Implemented:
    Because:

--- app.js
+++ app.js
@@ -31,8 +31,29 @@ function add_edit_task() {
     console.log("Second");
   }
 }
+//1st try-
 
 function get_todo_list() {
+  const card =
+    '<div class="card-wrapper" id="todo-card-wrapper"><div class="card-heading" id="todo-card-title">Wake me :-</div><div class="task-list"><p id="todo-card-description">Wake me up at 5o clock on monday.For walking</p></div><div class="grid-container-button"><div class="button-border" onclick="display_edit_form()"><i class="fas fa-edit"></i></div><div class="button-border" onclick="delete_card()"><i class="fas fa-trash-alt"></i></div></div></div>';
+
+  for (let i = 0; i < 10; i++) {
+    var container = document.createElement("div");
+    container.innerHTML = card;
+    document.getElementById("grid-card-container").appendChild(container);
+  }
+}
+
+/*
+function get_todo_list() {
+  let card = document.createElement("div");
+  card.classList = "card-wrapper";
+  element1 = "<h1>asfas</h1>";
+  document.getElementById("grid-card-container")[0].appendChild(card);
+}
+*/
+
+function get_todo_list1() {
   fetch("http://127.0.0.1:8000/view-all-todo", {
     "Content-type": "application/json",
   })
@@ -44,8 +65,21 @@ function get_todo_list() {
     })
     .then(function (data) {
       data.data.forEach((element) => {
+        const card = document.createElement("div");
+        card.classList.add("card-wrapper");
+        document.getElementById("grid-card-container").appendChild(card);
+
+        //let card = document.querySelector("#todo-card-wrapper");
+        /*
+        let card = document.getElementById("todo-card-wrapper");
+        console.log(card.childNodes);
+        document.getElementById("grid-card-container").appendChild(card);
+
+        // document
+        // .getElementById("grid-card-container")
         console.log(element.description);
         console.log(element.title);
+        */
       });
     })
     .catch(function (error) {
